6b797604436d1f7ed67f40356ae12c186668425d
Kompella Sai Manvish <57439628+manvish09@users.noreply.github.com>
2024-12-14 01:15:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//fetch is used to extract the data from the API
+fetch('https://emoji-api.com/emojis?access_key=634bc38fd77938fd7c49c4a716e834888c8e28c2')
+//.then is to represent once we have the data, what do we do then, and we are taking the result and converting it to JSON format.
+    .then(res => res.json())
+//once the data is loaded, we are calling the function upon this data that would perform some actions.
+    .then(data => loadEmoji(data))
+
+
+function loadEmoji(data){
+
+const btn = document.querySelector("#emoji");
+const name = document.querySelector("#emoji-name");
+const group = document.querySelector("#emoji-group");
+const subgroup = document.querySelector("#emoji-sg");
+btn.addEventListener("mouseover", ()=> {
+    const index = Math.floor(Math.random()*1859);
+    emoji.innerHTML = data[index].character;
+    name.innerHTML = "Name: "+ data[index].unicodeName.slice(5, data[index].unicodeName.length);;;
+    group.innerHTML = "Group: "+ data[index].group;
+    subgroup.innerHTML = "Sub-Group: " + data[index].subGroup;
+})
+    
+}
